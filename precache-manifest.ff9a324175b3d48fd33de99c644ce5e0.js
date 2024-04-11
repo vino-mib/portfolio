c78f8bf0024e0a08d13c267e134cb83e1cae54b7
Vinothkumar Chandrasekaran <vinothkumar.chandrasekaran@rabobank.nl>
2024-04-11 13:37:55 +0530

    initial commit

---
+++ precache-manifest.ff9a324175b3d48fd33de99c644ce5e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32bfa6ce0c32e3d0c9f02d46d02ae9e",
+    "url": "/vino-mib/index.html"
+  },
+  {
+    "revision": "1ea4845d4b49eac4faa2",
+    "url": "/vino-mib/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ab2615986a402ffb0a1c",
+    "url": "/vino-mib/static/css/main.89dfb679.chunk.css"
+  },
+  {
+    "revision": "1ea4845d4b49eac4faa2",
+    "url": "/vino-mib/static/js/2.848cfc82.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/vino-mib/static/js/2.848cfc82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2615986a402ffb0a1c",
+    "url": "/vino-mib/static/js/main.151851b0.chunk.js"
+  },
+  {
+    "revision": "27d431fc8ef342afe8ac",
+    "url": "/vino-mib/static/js/runtime-main.149f632b.js"
+  }
+]);
