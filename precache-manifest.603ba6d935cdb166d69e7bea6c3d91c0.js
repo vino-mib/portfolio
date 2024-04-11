1b7fbbdfa4b8586307279ce4c44e48b1163c67eb
Vinothkumar Chandrasekaran <vinothkumar.chandrasekaran@rabobank.nl>
2024-04-11 13:55:56 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.603ba6d935cdb166d69e7bea6c3d91c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c37f376074b79552f4c92df317e8ab6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "03480948283cfa0ee9e2",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b06d10407b4d6ef9b2a6",
+    "url": "/portfolio/static/css/main.89dfb679.chunk.css"
+  },
+  {
+    "revision": "03480948283cfa0ee9e2",
+    "url": "/portfolio/static/js/2.f341b38b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.f341b38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06d10407b4d6ef9b2a6",
+    "url": "/portfolio/static/js/main.00208a2b.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
