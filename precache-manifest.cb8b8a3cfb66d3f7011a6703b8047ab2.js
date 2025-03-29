4e2bfbe5a20d29c079ad9e4b085de28f4bac69b6
Vinothkumar Chandrasekaran <vinothkumar.chandrasekaran@rabobank.nl>
2025-03-29 21:37:22 +0530

    Updates

---
+++ precache-manifest.cb8b8a3cfb66d3f7011a6703b8047ab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f119600ae9ce040a3b0ea2cb4eae4b56",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ee55b38ce0b154221e6b",
+    "url": "/portfolio/static/css/2.ee6fb1dc.chunk.css"
+  },
+  {
+    "revision": "3375436aeafb3c4d09af",
+    "url": "/portfolio/static/css/main.60ea3606.chunk.css"
+  },
+  {
+    "revision": "ee55b38ce0b154221e6b",
+    "url": "/portfolio/static/js/2.1446e090.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/portfolio/static/js/2.1446e090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3375436aeafb3c4d09af",
+    "url": "/portfolio/static/js/main.87c731b5.chunk.js"
+  },
+  {
+    "revision": "58d18144e62526203a32",
+    "url": "/portfolio/static/js/runtime-main.770b1f5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.abd67bdee6f0728d4c0863ddb3fc5812.js"
+  "/portfolio/precache-manifest.cb8b8a3cfb66d3f7011a6703b8047ab2.js"
 );
 
 self.addEventListener('message', (event) => {
